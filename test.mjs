216bd31ec97506e1aaee005ec3afb7a903354c75
Shaw Summa <shawsumma@gmail.com>
2023-04-26 10:00:11 -0400

    ud

--- test.mjs
+++ test.mjs
@@ -1,30 +1,56 @@
-import * as parser from 'ebb/parser.js';
-import * as run from 'ebb/run.js';
+import {Env, isBad} from './temper.out/ebb/js/run.js';
+import * as process from 'process';
+import {readFileSync} from 'fs';
 
-const val = parser.parse(`
+const env = new Env();
 
-(print v) ?
+const tostr = (s) => {
+    let r = 0;
+    for (var i = s.length-1; i >= 0; i--) {
+        r = [s.charCodeAt(i), r];
+    }
+    return r;
+};
 
-(if cond (then) (else)) ?
+const fromstr = (s) => {
+    let r = '';
+    while (Array.isArray(s)) {
+        r += String.fromCharCode(s[0]);
+        s = s[1];
+    }
+    return r;
+};
 
-(lt x y) ?
+env.define("if", ([c, t, f]) => {
+    if (c !== 0) {
+        return t.invoke([]);
+    } else {
+        return f.invoke([]);
+    }
+});
 
-(sub x y) ?
-(add x y) ?
+env.define("putchar", ([c]) => {
+    process.stdout.write(String.fromCharCode(c));
+    return 0;
+});
 
-(do x y) y
+env.define("equal", ([a, b]) => (b === a)|0);
+env.define("above", ([a, b]) => (b < a) ? 1 : 0);
+env.define("add", ([a, b]) => b + a|0)
+env.define("sub", ([a, b]) => b - a|0)
+env.define("mul", ([a, b]) => b * a|0)
+env.define("div", ([a, b]) => b / a|0)
+env.define("mod", ([a, b]) => b % a|0)
+env.define("read-file", ([name]) => {
+    return tostr(String(readFileSync(fromstr(name))));
+});
+env.define("pair", ([a, b]) => [a, b]);
+env.define("first", ([p]) => p[0]);
+env.define("second", ([p]) => p[1]);
 
-(let [v] v (in v)) in v
-
-(fib n)
-if lt 2 n
-    n
-    add fib sub 2 n
-        fib sub 1 n
-
-(main)
-print fib 20
-
-`);
-
-run.run(val);
+env.source(String(readFileSync("ebb.ebb")));
+let a = [tostr(process.argv[2]), 0];
+const res = env.call("main", [a]);
+if (isBad(res)) {
+    console.log(res.msg);
+}
--- test.py
+++ test.py
@@ -1,37 +1,92 @@
 import logging
 import os
+import operator
+import time
+import sys
+
+sys.setrecursionlimit(10 ** 5)
 
 logging.basicConfig(filename="/dev/stdout", level=logging.DEBUG, format="%(message)s")
 
-import ebb.parser as parser
-import ebb.run as run
+from ebb.run import Env, is_bad
 
-val = parser.parse("""
+env = Env()
 
-(print v) ?
+def tostr(s):
+    r = 0
+    for i in s[::-1]:
+        r = (ord(i), r)
+    return r
 
-(if cond (then) (else)) ?
+def fromstr(s):
+    r = []
+    while isinstance(s, tuple):
+        r.append(chr(s[0]))
+        s = s[1]
+    return ''.join(r)
 
-(lt x y) ?
+def wrap(f):
+    def inner(args):
+        return f(*args[::-1])
+    return inner
 
-(sub x y) ?
-(add x y) ?
+def rwrap(f):
+    return lambda args: f(*args)
 
-(do x y) y
+def ffi_if(c, t, f):
+    if c:
+        return t.invoke(())
+    else:
+        return f.invoke(())
 
-(let [v] v (in v)) in v
+def ffi_read(f):
+    with open(fromstr(f)) as i:
+        return tostr(i.read())
+    
+def ffi_print(s):
+    if isinstance(s, tuple):
+        print(fromstr(s))
+    else:
+        print(s)
 
-(fib n)
-if lt 2 n
-    n
-    add fib sub 2 n
-        fib sub 1 n
+def ffi_input(s):
+    return input(fromstr(s))
 
-(main)
-print fib 20
+def ffi_putchar(c):
+    outc.write(chr(c))
 
-""")
+def ffi_pair(a, b):
+    return (a, b)
 
-run.run(val)
+def ffi_first(p):
+    return p[0]
+
+def ffi_second(p):
+    return p[1]
+
+env.define("if", rwrap(ffi_if))
+env.define("putchar", wrap(ffi_putchar))
+env.define("equal", wrap(operator.eq))
+env.define("above", wrap(operator.lt))
+env.define("add", wrap(operator.add))
+env.define("sub", wrap(operator.sub))
+env.define("mul", wrap(operator.mul))
+env.define("div", wrap(operator.floordiv))
+env.define("mod", wrap(operator.mod))
+env.define("read-file", wrap(ffi_read))
+env.define("pair", rwrap(ffi_pair))
+env.define("first", wrap(ffi_first))
+env.define("second", wrap(ffi_second))
+
+with open("ebb.ebb") as f:
+    env.source(f.read())
+
+a = (tostr(sys.argv[1]), 0)
+
+outc = sys.stdout
+
+res = env.call("main", (a,))
+if is_bad(res):
+    print(res.msg)
 
 os._exit(0)
